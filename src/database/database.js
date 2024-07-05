7776b4af42434c1b7b26575b3573a6cc3b388f5f
MECIOSLAPTOP\oceres <oceres@hotmail.com>
2024-07-04 21:05:52 -0400

    NODEJS-01: Fixed Variable

--- src/database/database.js
+++ src/database/database.js
@@ -6,7 +6,7 @@ export const sequelize = new Sequelize(
     process.env.DB_USER, //usere name
     process.env.DB_PASSWORD, //password
     {
-        hots: process.env.DB_HOST,
+        host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         logging: console.log,
         dialectOptions: {
